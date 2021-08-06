60153176b6c981ee516b725a7fb378c12930fa00
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2021-08-06 13:59:49 -0500

    java practice exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,31 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 42;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Hello";
+        System.out.println(myString);
+
+//        float myNumber = 3.14;
+//        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+//        String class;
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
     }
+
 }
